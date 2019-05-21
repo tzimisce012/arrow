cdebcc80f4842c66e14c9f1d3c153ebba0e98918
Simon Vergauwen <vergauwen.simon@gmail.com>
2019-05-21 16:23:33 +0200

    Update ForkFiber/ParMap benchmarks

--- modules/benchmarks/arrow-benchmarks-effects/src/jmh/kotlin/arrow/benchmarks/ForkFiber.kt
+++ modules/benchmarks/arrow-benchmarks-effects/src/jmh/kotlin/arrow/benchmarks/ForkFiber.kt
@@ -3,6 +3,10 @@ package arrow.benchmarks
 import arrow.effects.IO
 import arrow.effects.IODispatchers
 import arrow.effects.fix
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.CompilerControl
 import org.openjdk.jmh.annotations.Fork
@@ -23,14 +27,30 @@ open class ForkFiber {
   @Param("100")
   var size: Int = 0
 
+  private val arrowCtx = IODispatchers.CommonPool
+  private val kotlinxCtx = Dispatchers.Default
+
   private fun ioStartLoop(i: Int): IO<Int> =
     if (i < size) {
-      IO { i + 1 }.startFiber(IODispatchers.CommonPool).flatMap { fiber ->
+      IO.just(i + 1).startFiber(arrowCtx).flatMap { fiber ->
         fiber.join().fix().flatMap { ioStartLoop(it) }
       }
     } else IO.just(i)
 
+  private suspend fun kotlinxStartLoop(i: Int): Int =
+    if (i < size) {
+      coroutineScope {
+        val ii = withContext(kotlinxCtx) { i + 1 }
+        kotlinxStartLoop(ii)
+      }
+    } else i
+
   @Benchmark
   fun io(): Int =
     ioStartLoop(0).unsafeRunSync()
+
+  @Benchmark
+  fun kotlinx(): Int = runBlocking {
+    kotlinxStartLoop(0)
+  }
 }
--- modules/benchmarks/arrow-benchmarks-effects/src/jmh/kotlin/arrow/benchmarks/ParMap.kt
+++ modules/benchmarks/arrow-benchmarks-effects/src/jmh/kotlin/arrow/benchmarks/ParMap.kt
@@ -1,9 +1,15 @@
 package arrow.benchmarks
 
-import arrow.data.extensions.list.foldable.foldLeft
 import arrow.effects.IO
 import arrow.effects.IODispatchers
+import arrow.effects.IOOf
 import arrow.effects.extensions.io.concurrent.parMapN
+import arrow.effects.fix
+import arrow.effects.racePair
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
 import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.CompilerControl
 import org.openjdk.jmh.annotations.Fork
@@ -13,6 +19,7 @@ import org.openjdk.jmh.annotations.Scope
 import org.openjdk.jmh.annotations.State
 import org.openjdk.jmh.annotations.Warmup
 import java.util.concurrent.TimeUnit
+import kotlin.coroutines.CoroutineContext
 
 @State(Scope.Thread)
 @Fork(2)
@@ -24,12 +31,36 @@ open class ParMap {
   @Param("100")
   var size: Int = 0
 
-  private fun ioHelper(): IO<Int> =
-    (0 until size).toList().foldLeft(IO { 0 }) { acc, i ->
-      IODispatchers.CommonPool.parMapN(acc, IO { i }) { a, b -> a + b }
+  private val arrowCtx = IODispatchers.CommonPool
+  private val kotlinCtx = Dispatchers.Default
+
+  private fun ioHelper(): IO<Int> = (0 until size).fold(IO.just(1)) { acc, i ->
+    IO.parMapN(arrowCtx, acc, IO.just(i)) { a, b -> a + b }
+  }
+
+  private suspend fun kotlinxHelper(): Int = (0 until size).fold(1) { acc, i ->
+    coroutineScope {
+      val defA = async(kotlinCtx) { acc }
+      val defB = async(kotlinCtx) { i }
+      defA.await() + defB.await()
     }
+  }
 
   @Benchmark
   fun io(): Int =
     ioHelper().unsafeRunSync()
+
+  @Benchmark
+  fun kotlinx(): Int = runBlocking {
+    kotlinxHelper()
+  }
+
+  /** This will end up in IO.Companion in the coming weeks **/
+  fun <A, B, C> IO.Companion.parMapN(ctx: CoroutineContext, fa: IOOf<A>, fb: IOOf<B>, f: (A, B) -> C): IO<C> =
+    racePair(ctx, fa, fb).flatMap {
+      it.fold(
+        { (a, fiberB) -> fiberB.join().fix().map { b -> f(a, b) } },
+        { (fiberA, b) -> fiberA.join().fix().map { a -> f(a, b) } }
+      )
+    }
 }
